4cc666c7940cb2471dc1f7d8a635885c74427b85
KimInteger <kjs20151240@gmail.com>
2024-06-05 17:30:55 +0900

    res에 Access-Control-Allow-Origin을 세팅해봄 되지 아니함

--- app.js
+++ app.js
@@ -8,8 +8,28 @@ const path = require('node:path');
 
 const PORT = process.env.PORT || 3000;
 
+
+
 const server = http.createServer((req,res)=>{
-  console.log(1);
+  console.log(req.url);
+  res.setHeader('Access-Control-Allow-Origin' , `*`)
+  if(req.url === '/') {
+    fs.readFile('./public/index.html',(err,data)=>{
+      if(err){
+        throw err;
+      }
+      res.writeHead(200,{"Content-Type":"text/html;charset=UTF-8"});
+      res.end(data);
+    });
+  } else if (req.url==='/script.js') {
+    fs.readFile('./public/script.js',(err,data)=>{
+      if(err){
+        throw err;
+      }
+      res.writeHead(200,{"Content-Type":"application/javascript;charset=UTF-8"});
+      res.end(data);
+    });
+  } 
 });
 
 server.listen(PORT,(err)=>{
