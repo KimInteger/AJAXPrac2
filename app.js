3fce25a7bc5814e1facd1f66964d7d1b85d666f2
KimInteger <kjs20151240@gmail.com>
2024-06-05 14:25:44 +0900

    서버를 PORT에 연결하고 열림을 확인함

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ const fs = require('node:fs');
 const qs = require('node:querystring');
 
 const path = require('node:path');
+
+const PORT = process.env.PORT || 3000;
+
+const server = http.createServer((req,res)=>{
+  console.log(1);
+});
+
+server.listen(PORT,(err)=>{
+  if(err){
+    console.error('에러 발생 : ', err);
+  } else {
+    console.log("서버 가동중!");
+    console.log(`http://localhost:${PORT}`);
+  }
+})
