b4085bec4409786d5a0d8a33c3de3261e938657b
KimInteger <kjs20151240@gmail.com>
2024-06-05 17:31:49 +0900

    setRequestHeader로 권한을 설정해 보았으나 애초에 열쇠가 없는 상태로 문을따려고 했다는 것을 알게됨 종료

--- public/script.js
+++ public/script.js
@@ -1,3 +1,19 @@
 const root = document.getElementById('root');
 
 const list = document.getElementById('list');
+
+const xhr = new XMLHttpRequest();
+console.log('데이터를 받기 전 조회');
+console.dir(xhr);
+
+xhr.open('GET','https://seogu.go.kr/seoguAPI/3660000/getBakryStts?pageNo=1&numOfRows=10&type=json',true);
+
+xhr.setRequestHeader('Access-Control-Allow-Origin' , `*`);
+
+xhr.addEventListener('load',()=>{
+  if(xhr.status === 200){
+    console.log(1);
+  }
+});
+
+xhr.send();
