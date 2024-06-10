c1ecf94c7b07e954fcd3dbbbf1dd867c0b558064
KimInteger <kjs20151240@gmail.com>
2024-06-10 11:03:13 +0900

    데이터키조회 및 entryPoint획득

--- public/script.js
+++ public/script.js
@@ -2,13 +2,16 @@ const root = document.getElementById('root');
 
 const list = document.getElementById('list');
 
+const entryPoint = 'http://apis.data.go.kr/6300000/examNoticeService'
+
+const key = '4WZqKSFBKTKvEh0FmRpWZqAVCkO%2B2yz3ehUuYtqigHgFExfJZwCVJlXUJsqecScdYmrXgKD%2BU4tfMJql%2FHst%2Fg%3D%3D'
+
 const xhr = new XMLHttpRequest();
 console.log('데이터를 받기 전 조회');
 console.dir(xhr);
 
-xhr.open('GET','https://seogu.go.kr/seoguAPI/3660000/getBakryStts?pageNo=1&numOfRows=10&type=json',true);
-
-xhr.setRequestHeader('Access-Control-Allow-Origin' , `*`);
+xhr.open('GET',`http://apis.data.go.kr/6300000/examNoticeService/examNoticeList?serviceKey=	
+4WZqKSFBKTKvEh0FmRpWZqAVCkO%2B2yz3ehUuYtqigHgFExfJZwCVJlXUJsqecScdYmrXgKD%2BU4tfMJql%2FHst%2Fg%3D%3D&numOfRows=10&pageNo=1`,true);
 
 xhr.addEventListener('load',()=>{
   if(xhr.status === 200){
