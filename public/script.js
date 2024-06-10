8e92e8408e984a1d91a2cf117c444fa9e72baa31
KimInteger <kjs20151240@gmail.com>
2024-06-10 12:44:49 +0900

    마침내 받아온 데이터

--- public/script.js
+++ public/script.js
@@ -2,21 +2,20 @@ const root = document.getElementById('root');
 
 const list = document.getElementById('list');
 
-const entryPoint = 'http://apis.data.go.kr/6300000/examNoticeService'
-
-const key = '4WZqKSFBKTKvEh0FmRpWZqAVCkO%2B2yz3ehUuYtqigHgFExfJZwCVJlXUJsqecScdYmrXgKD%2BU4tfMJql%2FHst%2Fg%3D%3D'
-
 const xhr = new XMLHttpRequest();
 console.log('데이터를 받기 전 조회');
 console.dir(xhr);
 
-xhr.open('GET',`http://apis.data.go.kr/6300000/examNoticeService/examNoticeList?serviceKey=	
-4WZqKSFBKTKvEh0FmRpWZqAVCkO%2B2yz3ehUuYtqigHgFExfJZwCVJlXUJsqecScdYmrXgKD%2BU4tfMJql%2FHst%2Fg%3D%3D&numOfRows=10&pageNo=1`,true);
+xhr.open('GET',`http://apis.data.go.kr/5690000/sjGameStat/sj_00000100?pageIndex=1&pageUnit=20&dataTy=json&searchCondition=mtlty&serviceKey=4WZqKSFBKTKvEh0FmRpWZqAVCkO%2B2yz3ehUuYtqigHgFExfJZwCVJlXUJsqecScdYmrXgKD%2BU4tfMJql%2FHst%2Fg%3D%3D`,true);
 
 xhr.addEventListener('load',()=>{
-  if(xhr.status === 200){
-    console.log(1);
-  }
+  console.log('데이터 받은 후 조회');
+  console.dir(xhr);
+
+  let result = JSON.parse(xhr.responseText);
+
+  console.log(result);
+
 });
 
 xhr.send();
